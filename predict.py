73b6807c242cb740796d1af22aa758c35fd336ed
TXM-SB <43259319+TXM-SB@users.noreply.github.com>
2023-04-20 17:11:29 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,32 @@
+import time
+import os
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+from util.visualizer import Visualizer
+from pdb import set_trace as st
+from util import html
+
+opt = TestOptions().parse()
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+# 创建数据集loader，并加载数据集
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+model = create_model(opt)
+visualizer = Visualizer(opt)
+web_dir = os.path.join("./result/", opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+
+print(len(dataset))
+for i, data in enumerate(dataset):
+    model.set_input(data)
+    visuals = model.predict()
+    img_path = model.get_image_paths()
+    print('process image... %s' % img_path)
+    visualizer.save_images(webpage, visuals, img_path)
+
+webpage.save()
